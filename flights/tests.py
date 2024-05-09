b15f76da1f9157be9702a6cf7d2214156ac63f1c
Bryium <bryiumonyancha@gmail.com>
2024-05-09 18:28:21 +0300

    asserted true to test flight

--- flights/tests.py
+++ flights/tests.py
@@ -30,4 +30,5 @@ class FlightTestCase(TestCase):
     a1 = Airport.objects.get(code="AAA") 
     a2 = Airport.objects.get(code="BBB") 
     f = Flight.objects.get(origin=a1, destination=a2, duration=100)
+    self.assertTrue(f.is_valid_flight())
 
