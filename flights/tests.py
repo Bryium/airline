cfcd3e21e191bfe2fe8c6c7fcf987dc5cb15c6f4
Bryium <bryiumonyancha@gmail.com>
2024-05-09 19:03:03 +0300

    tested flight page

--- flights/tests.py
+++ flights/tests.py
@@ -50,3 +50,10 @@ class FlightTestCase(TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.context["flights"].count(), 3)
 
+  def test_valid_flight_page(self):
+    a1 = Airport.objects.get(code="AAA")
+    f = Flight.objects.get(origin=a1, destination=a1)
+
+    c = Client()
+    response = c.get(f"/flights/{f.id}")
+    self.assertEqual(response.status_code, 200)
